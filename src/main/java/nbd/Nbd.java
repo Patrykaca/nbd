51163750bd1c78e069cdc6e8e95d217aaa22ecc7
Patryk Kaca <patrykaca@gmail.com>
2020-12-01 20:18:32 +0100

    new project

--- src/main/java/nbd/Nbd.java
+++
@@ -1,8 +0,0 @@
-package nbd;
-
-
-class Nbd{
-    public static void main(String[] args) {
-        System.out.println("2137");
-    }
-}
---
+++ src/test/java/com/nierelacyjne/cassandra/springbootcassandrademo/SpringbootCassandraDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nierelacyjne.cassandra.springbootcassandrademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCassandraDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
